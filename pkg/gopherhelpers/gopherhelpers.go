7209f668290a53ac46809ea55ff0728b4cbdf66f
Philipp Neumann <PN@mtd-systems.de>
2024-12-11 15:06:35 +0100

    Identfunction for long test for gophermaps

--- pkg/gopherhelpers/gopherhelpers.go
+++ pkg/gopherhelpers/gopherhelpers.go
@@ -40,6 +40,24 @@ func FormatInfo(indent int, header string, content string) string {
 	return section
 }
 
+func FormatInfoForGophermap(indent int, header string, content string) string {
+	content = strings.ReplaceAll(content, "\n", "")
+	indentstring := strings.Repeat(" ", indent)
+	header = header + strings.Repeat(" ", indent-len(header))
+	words := strings.Fields(content)
+	section := ""
+	currentline := header
+	for _, word := range words {
+		if len(currentline)+len(word)+1 > MaxLine {
+			section += CreateGopherInfo(currentline)
+			currentline = indentstring
+		}
+		currentline += word + " "
+	}
+	section += CreateGopherInfo(currentline)
+	return section
+}
+
 func FormatForGopherMap(indent int, header string, content string) string {
 	content = strings.ReplaceAll(content, "\n", "")
 	header = header + strings.Repeat(" ", indent-len(header))
